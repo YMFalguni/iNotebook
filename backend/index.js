e220cec724118911945134e45a44c8fb596e6653
YMFalguni <falguniym@gmail.com>
2025-12-18 20:04:44 +0530

    Fix all issues: reset to working state

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,11 @@
 const connectToMongo = require('./db');
 const express = require('express')
-var cors = require('cors')
+const cors = require('cors')
 
 connectToMongo();
 const app = express()
 const port = 5000
 
-
-
-const cors = require("cors");
 app.use(cors({
   origin: "*",
   methods: ["GET", "POST", "PUT", "DELETE"],
--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -3,14 +3,12 @@ import { useState } from "react";
 import API_ENDPOINTS from "../../config/apiConfig";
 
 const NoteState = (props)=>{
-    // const host = process.env.REACT_APP_HOST || "http://localhost:5000";
-    // const host = "https://inotebook-2-yxk2.onrender.com";
-    const host = process.env.REACT_APP_HOST;  // something like your backend URL
+    const host = process.env.REACT_APP_HOST;
     const notesInitial = [];
 // GET ALL NOTES
     const getNotes = async ()=>{
 // API CALL
-      const response = await fetch(API_ENDPOINTS.GET_NOTES, {
+      const response = await fetch(`${host}/api/notes/fetchallnotes`, {
           method: 'GET',
             headers: {
                 'Content-Type': 'application/json',
@@ -26,7 +24,7 @@ const [notes, setNotes] = useState(notesInitial)
 
     const addNote = async ( title, description, tag)=>{
 // API CALL
-      const response = await fetch(API_ENDPOINTS.CREATE_NOTE, {
+      const response = await fetch(`${host}/api/notes/addnote`, {
         method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
@@ -42,7 +40,7 @@ const note = await response.json();
 // DELETE A NOTE    
 const deleteNote = async (id)=>{
     const response = 
-      await fetch(`${API_ENDPOINTS.DELETE_NOTE}/${id}`, {
+      await fetch(`${host}/api/notes/deletenote/${id}`, {
         method: 'DELETE',
             headers: {
                 'Content-Type': 'application/json',
@@ -50,42 +48,14 @@ const deleteNote = async (id)=>{
             }
         });
         const json = response.json();
-        console.log(json);
     const newNotes = notes.filter((note)=>{return note._id!==id});
     setNotes(newNotes);
     }
 
 // EDIT A NOTE
-
-//  const editNote = async (id , title, description, tag)=>{
-// // API CALL
-//         const response = await fetch (`${host}/api/notes/updatenote/${id}`, {method: 'PUT',
-//             headers: {
-//                 'Content-Type': 'application/json',
-//                 'auth-token': localStorage.getItem("token")
-//             },
-//             body: JSON.stringify({title, description, tag})
-//         });
-//         const json = await response.json();
-//         console.log(json)
-//  let newNotes = JSON.parse(JSON.stringify(notes));
-// //Logic to edit in client side
-//  for (let index = 0; index < newNotes.length; index++) {
-//     const element = newNotes[index];
-//     if (element._id === id) {
-//         newNotes[index].title = title;
-//         newNotes[index].description = description;
-//         newNotes[index].tag = tag;
-//          break;
-//     }
-// }
-//  console.log(newNotes)
-//  setNotes(newNotes);
-// }
-
 const editNote = async (id, title, description, tag) => {
   
-      const res = await fetch(`${API_ENDPOINTS.UPDATE_NOTE}/${id}`, {
+      const res = await fetch(`${host}/api/notes/updatenote/${id}`, {
       method: "PUT",
     headers: {
       "Content-Type": "application/json",
